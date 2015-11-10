6fc236bc84497652ff473468c45836af5ec46fe2
Bjørn Johansen <post@bjornjohansen.no>
2015-11-10 23:04:04 +0100

    Setup a minimal skeleton for the Instant_Articles_Post class which will be responsible for constructing our content and preparing it for rendering

---
+++ class-instant-articles-post.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class responsible for constructing our content and preparing it for rendering
+ *
+ * @since 0.1
+ */
+class Instant_Articles_Post {
+
+	/**
+	 * Setup data and build the content
+	 *
+	 * @since 0.1
+	 * @param int $post_id ID of the post
+	 */
+	function __construct( $post_id ) {
+
+	}
+
+}
+
