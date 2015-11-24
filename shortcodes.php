a9c88592617ccff39c0f0c65ad943e80dd709fa7
Trine <trine@dekode.no>
2015-11-24 13:04:56 +0100

    Add shortcode handler for Audio

--- shortcodes.php
+++ shortcodes.php
@@ -32,8 +32,9 @@ add_shortcode( 'caption', 'instant_articles_shortcode_handler_caption' );
 add_shortcode( 'wp_caption', 'instant_articles_shortcode_handler_caption' );
 
 /**
- * Gallery Shortcode
+ * Caption/WP-Caption Shortcode
  * @param  array     $atts       Array of attributes passed to shortcode.
+ * @param  string    $content    The content passed to the shortcode.
  * @return string                The generated content.
 */
 function instant_articles_shortcode_handler_caption( $atts, $content = "" ) {
@@ -42,7 +43,7 @@ function instant_articles_shortcode_handler_caption( $atts, $content = "" ) {
     $doc->loadHTML( $content );
     $imageTags = $doc->getElementsByTagName('img');
     $img_src = $imageTags[0]->getAttribute('src');
-    
+
 	ob_start(); ?>
 		<figure>
   			<img src="<?php echo esc_url ( $img_src ); ?>" />
@@ -51,4 +52,45 @@ function instant_articles_shortcode_handler_caption( $atts, $content = "" ) {
 		
 	<?php return ob_get_clean();
 }
+
+add_shortcode( 'audio', 'instant_articles_shortcode_handler_audio' );
+
+/**
+ * Audio Shortcode
+ * @param  array     $atts       Array of attributes passed to shortcode.
+ * @return string                The generated content.
+*/
+function instant_articles_shortcode_handler_audio( $atts ) {	
+
+	if ( $atts['mp3'] ) {
+		$audio_src =  $atts['mp3'];
+	} else if ( $atts['src'] ) {
+		$audio_src =  $atts['src'];
+	} else if ( $atts['ogg'] ) {
+		$audio_src =  $atts['ogg'];
+	} else if ( $atts['wav'] ) {
+		$audio_src =  $atts['wav'];
+	} else {
+		$audio_src = null;
+	}
+
+	//TODO: $set_autoplay = $atts['autoplay'] == 'true' ? ' autoplay' : '';
+
+	if ( $audio_src ) :
+		$file_url = array_reverse( explode( '/', $audio_src ) ) ;
+
+		ob_start(); ?>
+
+		<figure>
+			<audio title="<?php echo esc_html( $file_url[0] ); ?>">
+				<source src="<?php echo esc_url( $audio_src ); ?>">
+			</audio>
+		</figure>
+			
+		<?php return ob_get_clean();
+	endif;
+
+}
+
+
 
