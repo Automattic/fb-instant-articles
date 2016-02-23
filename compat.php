e476cd52168f81216bfeea229af489495382c6c3
Bjørn Johansen <bjorn@dekode.no>
2016-02-23 09:33:13 +0100

    Let others define constants to disable our compat layers (e.g. if they provide it themselves)

--- compat.php
+++ compat.php
@@ -1,21 +1,21 @@
 <?php
 
 // Load compat layer for Co-Authors Plus
-if ( function_exists( 'get_coauthors' ) ) {
+if ( function_exists( 'get_coauthors' ) && ! defined( 'CAP_IA_COMPAT' ) ) {
 	include(  dirname( __FILE__ ) . '/compat/class-instant-articles-co-authors-plus.php' );
 	$cap = new Instant_Articles_Co_Authors_Plus;
 	$cap->init();
 }
 
 // Load compat layer for Yoast SEO
-if ( defined( 'WPSEO_VERSION' ) ) {
+if ( defined( 'WPSEO_VERSION' ) && ! defined( 'WPSEO_IA_COMPAT' ) ) {
 	include(  dirname( __FILE__ ) . '/compat/class-instant-articles-yoast-seo.php' );
 	$yseo = new Instant_Articles_Yoast_SEO;
 	$yseo->init();
 }
 
 // Load support for Google Analytics for WordPress (Google Analytics by Yoast)
-if ( defined( 'GAWP_VERSION' ) ) {
+if ( defined( 'GAWP_VERSION' ) && ! defined( 'GAWP_IA_COMPAT' ) ) {
 	include( dirname( __FILE__ ) . '/compat/class-instant-articles-google-analytics-for-wordpress.php' );
 	$gawp = new Instant_Articles_Google_Analytics_For_WordPress;
 	$gawp->init();
