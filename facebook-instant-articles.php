7dbb280cbf6af47b0636aa204fcb4895389080a8
diegoquinteiro <diegoquinteiro@fb.com>
2016-04-12 18:06:08 -0300

    Bump version to v2.5

--- facebook-instant-articles.php
+++ facebook-instant-articles.php
@@ -4,7 +4,7 @@
  * Description: Add support for Instant Articles for Facebook to your WordPress site.
  * Author: Automattic, Dekode, Facebook
  * Author URI: https://vip.wordpress.com/plugins/instant-articles/
- * Version: 2.4
+ * Version: 2.5
  * Text Domain: instant-articles
  * License: GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
