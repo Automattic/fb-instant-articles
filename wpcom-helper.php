003e228c82a7cfb466cbe3f4c695f43ecf467128
Philip John <phil@philipjohn.co.uk>
2016-04-25 13:16:35 +0100

    Track FBIA as normal page views on wpcom

--- wpcom-helper.php
+++ wpcom-helper.php
@@ -27,7 +27,7 @@ function _wpcom_fbia_stats_pixel( $content ) {
 	if( ! is_feed() )
 		return $content;
 
-	$url = 'https://pixel.wp.com/b.gif?host=' . $_SERVER[ 'HTTP_HOST' ] . '&blog=' . $current_blog->blog_id . '&post=' . $post->ID . '&subd=' . str_replace( '.wordpress.com', '', $current_blog->domain ) . '&ref=&feed=1';
+	$url = 'https://pixel.wp.com/b.gif?host=' . $_SERVER[ 'HTTP_HOST' ] . '&blog=' . $current_blog->blog_id . '&post=' . $post->ID . '&subd=' . str_replace( '.wordpress.com', '', $current_blog->domain ) . '&ref=';
 
 	$fbia_pixel = '
 <figure class="op-tracker">
