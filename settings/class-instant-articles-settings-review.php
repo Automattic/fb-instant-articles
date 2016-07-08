67dde90e8bdac0ee6622cd1d62df0a657f408c98
Diego Moreno Quinteiro <diegoquinteiro@gmail.com>
2016-07-07 22:13:09 -0300

    Show review status

--- settings/class-instant-articles-settings-review.php
+++ settings/class-instant-articles-settings-review.php
@@ -7,7 +7,7 @@
  * @package default
  */
 
-use Facebook\PersistentData\PersistentDataInterface;
+use Facebook\InstantArticles\Client\Client;
 
 /**
  * Class responsible for functionality and rendering of the settings
@@ -15,5 +15,28 @@ use Facebook\PersistentData\PersistentDataInterface;
  * @since 0.4
  */
 class Instant_Articles_Settings_Review {
+
+	public static function getReviewSubmissionStatus() {
+
+		$fb_app_settings = Instant_Articles_Option_FB_App::get_option_decoded();
+		$fb_page_settings = Instant_Articles_Option_FB_Page::get_option_decoded();
+
+		if ( isset( $fb_app_settings['app_id'] )
+			&& isset( $fb_app_settings['app_secret'] )
+			&& isset( $fb_page_settings['page_access_token'] )
+			&& isset( $fb_page_settings['page_id'] ) ) {
+
+			$client = Client::create(
+				$fb_app_settings['app_id'],
+				$fb_app_settings['app_secret'],
+				$fb_page_settings['page_access_token'],
+				$fb_page_settings['page_id'],
+				false
+			);
+
+			return $client->getReviewSubmissionStatus();
+		}
+		return null;
+	}
 
 }
--- settings/template-settings-review.php
+++ settings/template-settings-review.php
@@ -7,28 +7,34 @@
  * @package default
  */
 ?>
+<?php $status = Instant_Articles_Settings_Review::getReviewSubmissionStatus(); ?>
+
+<?php switch ( $status ) :
+	case 'REJECTED': ?>
 <pre>
 Your site was rejected. Please read the review feedback [here], fix the reported issues
 and submit it for review again.
 
 [ Submit for review ]
-
---
-
+</pre>
+<?php break; ?>
+<?php case 'APPROVED': ?>
+<pre>
 Your site was approved. All new articles will be automatically pushed to Instant Articles.
-
---
-
+</pre>
+<?php break; ?>
+<?php case 'PENDING': ?>
+<pre>
 Your site is currently under review.
-
---
-
+</pre>
+<?php break; ?>
+<?php case 'NOT_SUBMITTED': ?>
+<pre>
 Progress: 5 [#############################################] 10
 
 [ Submit for review ]
-
---
-
+</pre>
+<pre>
 Progress: 5 [#####################------------------------] 10
 
 Articles not on Instant Articles:
@@ -43,3 +49,4 @@ Title                      | Submit to Instant Articles
 
 You need to submit 5 more articles before review.
 </pre>
+<?php endswitch ?>
