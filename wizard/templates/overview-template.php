dfaf9f70678dce2a4c1e600f029e5538b8efa018
Everton Rosario <erosario@gmail.com>
2016-07-26 17:12:01 -0300

    Layout of first step

--- wizard/templates/overview-template.php
+++ wizard/templates/overview-template.php
@@ -1,1 +1,21 @@
 <h2>Overview</h2>
+<div class="instant-articles-card">
+  <div class="instant-articles-card-title">
+    <h3>Authenticate Plugin</h3>
+    <span>Step 1 of 2</span>
+  </div>
+  <div class="instant-articles-card-content">
+    <div class="instant-articles-card-content-box instant-articles-card-content-left">
+      <label>Title of this section</label>
+      <p>Yo dawg, you need a Facebook App to enable this plugin. It's for really cool security reasons. If you already know what I'm sayin' and have an App ID and App Secret, <a href="#">go here</a> to grab it. If you don't have any idea what's going on then go ahead and get an App ID already.</p>
+      <button class="instant-articles-button">Get App ID</button>
+    </div>
+    <div class="instant-articles-card-content-box instant-articles-card-content-right">
+      <label class="instant-articles-label">App ID</label>
+      <input class="instant-articles-input-text"type="text"/>
+      <label class="instant-articles-label">App Secret</label>
+      <input class="instant-articles-input-text" type="text"/>
+      <button class="instant-articles-button instant-articles-button-highlight instant-articles-button-disabled">Submit</button>
+    </div>
+  </div>
+</div>
