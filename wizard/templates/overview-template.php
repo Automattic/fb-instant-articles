2f6b0bbfc0a66bcc67b0ad4fc6b15b90cce762df
Everton Rosario <erosario@gmail.com>
2016-07-27 15:20:20 -0300

    Added step 2 and the final step

--- wizard/templates/overview-template.php
+++ wizard/templates/overview-template.php
@@ -2,7 +2,62 @@
 <div class="instant-articles-card">
   <div class="instant-articles-card-title">
     <h3>Authenticate Plugin</h3>
-    <span>Step 1 of 2</span>
+    <div class="instant-articles-card-title-right">
+      <span class="instant-articles-card-title-checkmark">✔</span>
+      <label class="instant-articles-card-title-label">App connected:</label>
+      <span class="instant-articles-card-title-value">1273677375983360</span>
+      <a class="instant-articles-card-title-edit" href="#"></a>
+      <span class="instant-articles-card-title-step">Step 1 of 2</span>
+    </div>
+  </div>
+  <div class="instant-articles-card-content">
+    <div class="instant-articles-card-content-box instant-articles-card-content-left">
+      <label>Title of this section</label>
+      <p>Yo dawg, you need a Facebook App to enable this plugin. It's for really cool security reasons. If you already know what I'm sayin' and have an App ID and App Secret, <a href="#">go here</a> to grab it. If you don't have any idea what's going on then go ahead and get an App ID already.</p>
+      <button class="instant-articles-button">Get App ID</button>
+    </div>
+    <div class="instant-articles-card-content-box instant-articles-card-content-right">
+      <label class="instant-articles-label">App ID</label>
+      <input class="instant-articles-input-text"type="text"/>
+      <label class="instant-articles-label">App Secret</label>
+      <input class="instant-articles-input-text" type="text"/>
+      <button class="instant-articles-button instant-articles-button-highlight instant-articles-button-disabled">Submit</button>
+    </div>
+  </div>
+</div>
+
+<div class="instant-articles-card">
+  <div class="instant-articles-card-title">
+    <h3>Authenticate Plugin</h3>
+    <div class="instant-articles-card-title-right">
+      <span class="instant-articles-card-title-checkmark">✔</span>
+      <label class="instant-articles-card-title-label">App connected:</label>
+      <span class="instant-articles-card-title-value">1273677375983360</span>
+      <a class="instant-articles-card-title-edit" href="#"></a>
+      <span class="instant-articles-card-title-step">Step 2 of 2</span>
+    </div>
+  </div>
+  <div class="instant-articles-card-content">
+    <div class="instant-articles-card-content-box instant-articles-card-content-left">
+      <span class="instant-articles-card-title-checkmark">✔</span>
+      <label class="instant-articles-card-title-label">App connected:</label>
+      <span class="instant-articles-card-title-value">1273677375983360</span>
+      <a class="instant-articles-card-title-edit" href="#"></a>
+    </div>
+  </div>
+</div>
+
+
+<div class="instant-articles-card instant-articles-card-collapsed">
+  <div class="instant-articles-card-title">
+    <h3>Authenticate Plugin</h3>
+    <div class="instant-articles-card-title-right">
+      <span class="instant-articles-card-title-checkmark">✔</span>
+      <label class="instant-articles-card-title-label">App connected:</label>
+      <span class="instant-articles-card-title-value">1273677375983360</span>
+      <a class="instant-articles-card-title-edit" href="#"></a>
+      <span class="instant-articles-card-title-step">Step 1 of 2</span>
+    </div>
   </div>
   <div class="instant-articles-card-content">
     <div class="instant-articles-card-content-box instant-articles-card-content-left">
