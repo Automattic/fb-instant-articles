fb1f6073a15a66b3478db5c2ec2df38cd28d5f27
philipjohn <phil@philipjohn.me.uk>
2016-08-04 14:20:43 +0100

    Fix the plugin include in the tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,7 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/instant-articles.php';
+	require dirname( dirname( __FILE__ ) ) . '/facebook-instant-articles.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
