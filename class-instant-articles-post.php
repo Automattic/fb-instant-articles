807ccfa86802bfbb6acea530a556415026c16ba1
Yohei Sasaki <yohei@fb.com>
2016-08-04 16:58:16 +0900

    get_the_title() method should return the title filtered by 'the_title'.
    This is required to output the exactly same title into Instant Article.
    
    The issue like #372 can be fixed by this commit since other plugings
    do something on the post title using this filter.

--- class-instant-articles-post.php
+++ class-instant-articles-post.php
@@ -85,6 +85,14 @@ class Instant_Articles_Post {
 		$title = $this->_post->post_title;
 
 		/**
+		 * Apply the default filter 'the_title' for the post tite.
+		 *
+		 * @since 3.1
+		 * @param string  $title  The current post title.
+		 */
+		$title = apply_filters( 'the_title', $title);
+
+		/**
 		 * Filter the post title for use in instant articles.
 		 *
 		 * @since 0.1
@@ -327,7 +335,7 @@ class Instant_Articles_Post {
 	        foreach ( $matches[0] as $link ) {
 	                $content = str_replace( $link, strip_tags($link), $content );
 	        }
-		
+
 		/**
 		 * Filter the post content for Instant Articles.
 		 *
