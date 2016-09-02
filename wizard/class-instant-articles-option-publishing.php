54f82efb3533cfd6334a2fe84889377b5979e59c
Everton Rosario <erosario@gmail.com>
2016-09-02 15:11:38 -0300

    Flips to block publishing of articles with warning

--- wizard/class-instant-articles-option-publishing.php
+++ wizard/class-instant-articles-option-publishing.php
@@ -51,7 +51,7 @@ class Instant_Articles_Option_Publishing extends Instant_Articles_Option {
 			'label' => 'Transformation warnings',
 			'description' => 'Articles won\'t be available as Instant Articles if transformation process contains warnings. Note: All warnings should be fixed in order to have them available as Instant Articles.',
 			'render' => 'checkbox',
-			'default' => false,
+			'default' => true,
 			'checkbox_label' => 'Don\'t publish with warnings',
 		),
 	);
