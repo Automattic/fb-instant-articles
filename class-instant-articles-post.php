5c24b3ccc0859e5f9992529ffc5e48339b5532a3
Everton Rosario <erosario@gmail.com>
2016-10-05 14:02:28 -0300

    Escape url inline

--- class-instant-articles-post.php
+++ class-instant-articles-post.php
@@ -703,8 +703,7 @@ class Instant_Articles_Post {
 				if ( ! empty( $src ) ) {
 					$explode_src = parse_url( $src );
 					if ( is_array( $explode_src ) && empty( $explode_src['scheme'] ) && ! empty( $explode_src['host'] ) && ! empty( $explode_src['path'] ) ) {
-						$src = 'https://' . $explode_src['host'] . $explode_src['path'];
-						$src = esc_url( $src );
+						$src = esc_url( 'https://' . $explode_src['host'] . $explode_src['path'] );
 					}
 					$script->setAttribute( 'src' , $src );
 				}
