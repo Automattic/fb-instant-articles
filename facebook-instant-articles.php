15283f7573c23953a0aecf229724dacb8168d761
Everton Rosario <erosario@gmail.com>
2016-11-08 10:36:21 -0200

    Removed unused resources

--- facebook-instant-articles.php
+++ facebook-instant-articles.php
@@ -75,7 +75,6 @@ if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
 	require_once( dirname( __FILE__ ) . '/embeds.php' );
 	require_once( dirname( __FILE__ ) . '/class-instant-articles-post.php' );
 	require_once( dirname( __FILE__ ) . '/wizard/class-instant-articles-wizard.php' );
-	require_once( dirname( __FILE__ ) . '/wizard/class-instant-articles-setup.php' );
 	require_once( dirname( __FILE__ ) . '/meta-box/class-instant-articles-meta-box.php' );
 	require_once( dirname( __FILE__ ) . '/class-instant-articles-publisher.php' );
 
