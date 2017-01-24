1f348839d7c93c588a6c91d73824d73a5ad66276
Abdus Fauzi <cola.sced@gmail.com>
2017-01-25 04:13:42 +0800

    Update embeds.php
    fix indent to tabs

--- embeds.php
+++ embeds.php
@@ -41,11 +41,11 @@ function instant_articles_embed_oembed_html( $html, $url, $attr, $post_ID ) {
 		$provider_name = 'twitter';
 	} elseif( false !== strpos( $providerURL, 'youtube.com' ) ) {
 		$provider_name = 'youtube';
-    } elseif( false !== strpos( $providerURL, 'vimeo.com' ) ) {
-        $provider_name = 'vimeo';
+	} elseif( false !== strpos( $providerURL, 'vimeo.com' ) ) {
+		$provider_name = 'vimeo';
 	} elseif( false !== strpos( $providerURL, 'vine.co' ) ) {
 		$provider_name = 'vine';
-    } elseif( false !== strpos( $providerURL, 'facebook.com' ) ) {
+	} elseif( false !== strpos( $providerURL, 'facebook.com' ) ) {
 		$provider_name = 'facebook';
 	}
 
