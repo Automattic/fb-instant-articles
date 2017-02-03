c9dc86ac52fe679708aea2df17478d450ab854b9
Alvin De Cruz <decruz@gmail.com>
2017-02-03 11:31:54 +0800

    changed html comment text

--- class-instant-articles-post.php
+++ class-instant-articles-post.php
@@ -760,7 +760,7 @@ class Instant_Articles_Post {
 			case 'embed':
 				if ( ! empty( $settings_ads['embed_code'] ) ) {
 					$sectionAds = $settings_ads['embed_code'];
-					$tokenizeAds = explode("<!-- Embed Ad Block -->", $sectionAds);
+					$tokenizeAds = explode("<!-- adblock -->", $sectionAds);
 					
 					foreach ($tokenizeAds as $advert) {
 						$ad = Ad::create()
