af398c3700526e2299e0f326ac877334bcdc7473
Diego Moreno Quinteiro <diegoquinteiro@fb.com>
2017-03-16 14:07:58 -0300

    Further cleanup of unused code

--- wizard/class-instant-articles-wizard.php
+++ wizard/class-instant-articles-wizard.php
@@ -8,13 +8,9 @@
  */
 require_once( dirname( __FILE__ ) . '/class-instant-articles-option-ads.php' );
 require_once( dirname( __FILE__ ) . '/class-instant-articles-option-analytics.php' );
-require_once( dirname( __FILE__ ) . '/class-instant-articles-option-fb-app.php' );
 require_once( dirname( __FILE__ ) . '/class-instant-articles-option-fb-page.php' );
 require_once( dirname( __FILE__ ) . '/class-instant-articles-option-publishing.php' );
 require_once( dirname( __FILE__ ) . '/class-instant-articles-option-styles.php' );
-require_once( dirname( __FILE__ ) . '/class-instant-articles-wizard-state.php' );
-require_once( dirname( __FILE__ ) . '/class-instant-articles-wizard-fb-helper.php' );
-require_once( dirname( __FILE__ ) . '/class-instant-articles-wizard-review-submission.php' );
 
 use Facebook\InstantArticles\Client\Client;
 use Facebook\InstantArticles\Client\ClientException;
@@ -33,38 +29,12 @@ class Instant_Articles_Wizard {
 		add_filter( 'plugin_action_links_' . IA_PLUGIN_PATH, array( 'Instant_Articles_Wizard', 'add_settings_link_to_plugin_actions' ) );
 
 		add_action( 'admin_init', function () {
-			new Instant_Articles_Option_FB_App();
 			new Instant_Articles_Option_FB_Page();
 			new Instant_Articles_Option_Styles();
 			new Instant_Articles_Option_Ads();
 			new Instant_Articles_Option_Analytics();
 			new Instant_Articles_Option_Publishing();
 		});
-
-		add_action(
-			'wp_ajax_instant_articles_wizard_transition',
-			array( 'Instant_Articles_Wizard', 'transition' )
-		);
-
-		add_action(
-			'wp_ajax_instant_articles_wizard_save_app',
-			array( 'Instant_Articles_Wizard', 'save_app' )
-		);
-
-		add_action(
-			'wp_ajax_instant_articles_wizard_edit_app',
-			array( 'Instant_Articles_Wizard', 'edit_app' )
-		);
-
-		add_action(
-			'wp_ajax_instant_articles_wizard_is_page_signed_up',
-			array( 'Instant_Articles_Wizard', 'is_page_signed_up' )
-		);
-
-		add_action(
-			'wp_ajax_instant_articles_wizard_submit_for_review',
-			array( 'Instant_Articles_Wizard', 'submit_for_review' )
-		);
 
 	}
 
@@ -106,138 +76,6 @@ class Instant_Articles_Wizard {
 		return $url['host'];
 	}
 
-	public static function transition() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_html( 'You do not have sufficient permissions to access this page.' ) );
-		}
-
-		$new_state = sanitize_text_field( $_POST[ 'new_state' ] );
-
-		$params = $_POST[ 'params' ];
-		$params = json_decode( stripslashes( $params ), true );
-		if ( $params && !empty( $params ) ) {
-			foreach ( $params as $key => $param ) {
-				// escape every key
-				$params[ $key ] = sanitize_text_field( $param );
-			}
-		}
-		else {
-			$params = array();
-		}
-
-		try {
-			Instant_Articles_Wizard_State::do_transition( $new_state, $params );
-		} catch ( Exception $e ) {
-			// If something went wrong, simply render the error + the same state.
-			echo '<div class="error settings-error notice is-dismissible"><p><strong>' . esc_html( $e->getMessage() ) . '</strong></p></div>';
-		}
-
-		self::render( true );
-		die();
-	}
-
-	/**
-	 * Saves the App ID and App Secret.
-	 *
-	 * That does not trigger a state transition, as the state transition will
-	 * only happen when the user logs in and we grab the access token.
-	 */
-	public static function save_app() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_html( 'You do not have sufficient permissions to access this page.' ) );
-		}
-
-		$current_state = Instant_Articles_Wizard_State::get_current_state();
-		if ( $current_state !== Instant_Articles_Wizard_State::STATE_APP_SETUP ) {
-			die();
-		}
-
-		$app_id = sanitize_text_field( $_POST[ 'app_id' ] );
-		$app_secret = sanitize_text_field( $_POST[ 'app_secret' ] );
-
-		Instant_Articles_Option_FB_App::update_option( array(
-			'app_id' => $app_id,
-			'app_secret' => $app_secret
-		) );
-
-		self::render( true );
-		die();
-	}
-
-	/**
-	 * Submits the select page for review
-	 */
-	public static function submit_for_review() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_html( 'You do not have sufficient permissions to access this page.' ) );
-		}
-
-		$current_state = Instant_Articles_Wizard_State::get_current_state();
-		if ( $current_state !== Instant_Articles_Wizard_State::STATE_REVIEW_SUBMISSION ) {
-			die();
-		}
-
-		$fb_app_settings = Instant_Articles_Option_FB_App::get_option_decoded();
-		$fb_page_settings = Instant_Articles_Option_FB_Page::get_option_decoded();
-
-		try {
-			$client = Client::create(
-				$fb_app_settings[ 'app_id' ],
-				$fb_app_settings[ 'app_secret' ],
-				$fb_page_settings[ 'page_access_token' ],
-				$fb_page_settings[ 'page_id' ]
-			);
-
-			// Bulk upload articles for review
-			$articles_for_review = Instant_Articles_Wizard_Review_Submission::getArticlesForReview();
-			foreach ( $articles_for_review as $post ) {
-				Instant_Articles_Publisher::submit_article( $post->ID, $post );
-			}
-
-			// Trigger review submission
-			$client->submitForReview();
-		} catch ( Exception $e ) {
-			// If something went wrong, simply render the error + the same state.
-			echo '<div class="error settings-error notice is-dismissible"><p><strong>' . esc_html( $e->getMessage() ) . '</strong></p></div>';
-		}
-
-		self::render( true );
-		die();
-	}
-
-	/**
-	 * Edits the App ID and App Secret within the APP_SETUP state.
-	 */
-	public static function edit_app() {
-		if ( ! current_user_can( 'manage_options' ) ) {
-			wp_die( esc_html( 'You do not have sufficient permissions to access this page.' ) );
-		}
-
-		$current_state = Instant_Articles_Wizard_State::get_current_state();
-		if ( $current_state !== Instant_Articles_Wizard_State::STATE_APP_SETUP ) {
-			die();
-		}
-
-		Instant_Articles_Option_FB_App::delete_option();
-
-		self::render( true );
-		die();
-	}
-
-	public static function is_page_signed_up() {
-		$page_id = sanitize_text_field( $_POST[ 'page_id' ] );
-
-		$fb_helper = new Instant_Articles_Wizard_FB_Helper();
-		$pages = $fb_helper->get_pages();
-
-		if ( isset( $pages[ $page_id ] ) && $pages[ $page_id ][ 'supports_instant_articles' ] ) {
-			die( 'yes' );
-		}
-		else {
-			die( 'no' );
-		}
-	}
-
 	public static function render( $ajax = false ) {
 		if ( ! current_user_can( 'manage_options' ) ) {
 			wp_die( esc_html( 'You do not have sufficient permissions to access this page.' ) );
@@ -245,10 +83,7 @@ class Instant_Articles_Wizard {
 
 		try {
 			// Read options (they are used on the templates)
-			$current_state = Instant_Articles_Wizard_State::get_current_state();
 			$fb_page_settings = Instant_Articles_Option_FB_Page::get_option_decoded();
-			$fb_app_settings = Instant_Articles_Option_FB_App::get_option_decoded();
-			$fb_helper = new Instant_Articles_Wizard_FB_Helper();
 			$settings_url = self::get_url();
 
 			include( dirname( __FILE__ ) . '/templates/advanced-template.php' );
