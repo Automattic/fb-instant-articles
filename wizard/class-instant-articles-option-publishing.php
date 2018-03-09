59f578325709b288c634386597549dd957868723
Shannon Graham <shannon@rocketships.ca>
2018-03-09 10:54:13 -0800

    Added tranformation warning indicator column
    This branch adds a column to wp_edit pages called FBIA Status. The column shows a green light if the post was transformed into a Facebook Instant article with no problems, a yellow light if there were warnings, and a red light if the post was empty after trasnformation.
    
    There is a checkbox in the FBIA Options page that defaults to false, so that editors can decide whether to show the column or not. The checkbox is placed alongside the "Publish with warnings" option for good grouping of concerns.
    
    A new stylesheet has been added to contain the styling rules for the warning indicators. Since the indicators are very minimal, just red, green, and yellow lights, an informative title text is available on mouseover to help with accessibility.

--- wizard/class-instant-articles-option-publishing.php
+++ wizard/class-instant-articles-option-publishing.php
@@ -55,6 +55,14 @@ class Instant_Articles_Option_Publishing extends Instant_Articles_Option {
 			'checkbox_label' => 'Publish articles containing warnings',
 		),
 
+		'display_warning_column' => array(
+			'label' => 'Transformation warning column',
+			'description' => 'With this option enabled, a column will be added to post indexes to quickly see whether an article transformation failed, succeeded, or had warnings.',
+			'render' => 'checkbox',
+			'default' => false,
+			'checkbox_label' => 'Show column',
+		),
+
 		'likes_on_media' => array(
 			'label' => 'Likes',
 			'description' => 'With this option enabled, any image or video will have the like action enabled by default.',
