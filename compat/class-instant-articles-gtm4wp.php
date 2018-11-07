2ac1c81df619cb45207b27b61d96b6c986af04e7
Paul Schreiber <paul.schreiber@espn.com>
2018-11-06 23:40:40 -0500

    use snake_case for variable names

--- compat/class-instant-articles-gtm4wp.php
+++ compat/class-instant-articles-gtm4wp.php
@@ -33,9 +33,9 @@ class Instant_Articles_Google_Tag_Manager_For_WordPress {
 		);
 	}
 
-	public function add_ia_status_to_data_layer( $dataLayer ) {
-		$dataLayer['instantArticle'] = \is_transforming_instant_article();
+	public function add_ia_status_to_data_layer( $data_layer ) {
+		$data_layer['instantArticle'] = \is_transforming_instant_article();
 
-		return $dataLayer;
+		return $data_layer;
 	}
 }
